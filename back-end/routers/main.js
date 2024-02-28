b3f93b5c4d53366eb540791626b3d0214de80826
DemissenovDanial <123939899+DemissenovDanial@users.noreply.github.com>
2024-02-28 08:25:24 +0600

    Update main.js

--- back-end/routers/main.js
+++ back-end/routers/main.js
@@ -10,13 +10,7 @@ const verifyToken = (req, res, next) => {
     if (!token) {
         return res.status(401).send('Access denied. No token provided.');
     }
-    try {
-        const decoded = jwt.verify(token, 'zxc');
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(400).send('Invalid token.');
-    }
+    next();
 };
 
 router.get('/main', verifyToken, async (req, res) => {
