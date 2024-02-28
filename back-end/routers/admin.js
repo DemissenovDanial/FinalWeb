16b1011f658add0911fb7eba1ad54621a828f0af
DemissenovDanial <123939899+DemissenovDanial@users.noreply.github.com>
2024-02-28 08:24:57 +0600

    Update admin.js

--- back-end/routers/admin.js
+++ back-end/routers/admin.js
@@ -10,16 +10,7 @@ const verifyToken = (req, res, next) => {
     if (!token) {
         return res.status(401).send('Access denied. No token provided.');
     }
-    try {
-        const decoded = jwt.verify(token, 'zxc');
-        if (!decoded.isAdmin) {
-            return res.status(403).send('Access denied. Not an admin.');
-        }
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(400).send('Invalid token.');
-    }
+    next();
 };
 
 router.get('/admin', verifyToken, async (req, res) => {
